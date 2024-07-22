c548aa2142bdb1b85b5aef96dd6aa02f8d364787
FernandoParedesx <fpparedes2@espe.edu.ec>
2024-07-22 01:08:41 -0500

    Clear Proyect

--- app/msvc-cursos/src/main/java/com/espe/msvc/cursos/repositories/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.espe.msvc.cursos.repositories;
-
-import com.espe.msvc.cursos.models.entity.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> { }
